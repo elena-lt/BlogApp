d3ef355695d69d38992abe062c9e74c425645958
Elena Lutsiuk <elenalutsiuk5@gmail.com>
2021-08-06 22:55:26 +0200

    navigation setup

--- app/src/main/java/com/blogapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/blogapp/ui/main/MainActivity.kt
@@ -3,27 +3,55 @@ package com.blogapp.ui.main
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.NavGraphNavigator
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.blogapp.R
 import com.blogapp.ui.base.BaseActivity
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseActivity() {
+
+    private lateinit var navController: NavController
+//    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavGraph()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNavigationView.setupWithNavController(navController)
+
+        bottomNavigationView.setOnItemReselectedListener {
+            navController.currentDestination.
+
+        }
 
         subscribeToObservers()
-
-        findViewById<Button>(R.id.logout_button).setOnClickListener {
-            sessionManager.logout()
-        }
+//
+//        findViewById<Button>(R.id.logout_button).setOnClickListener {
+//            sessionManager.logout()
+//        }
     }
 
-    fun subscribeToObservers(){
-        sessionManager.cashedToken.observe(this, {authToken ->
-            Log.d ("MAIN_ACTIVITY", "authToken is $authToken")
-            if (authToken == null || authToken.account_primary_key ==-1 || authToken.token == null){
-                Log.d ("MAIN_ACTIVITY", "Navigating to auth activity")
+    private fun setupNavGraph() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+
+    private fun subscribeToObservers() {
+        sessionManager.cashedToken.observe(this, { authToken ->
+            Log.d("MAIN_ACTIVITY", "authToken is $authToken")
+            if (authToken == null || authToken.account_primary_key == -1 || authToken.token == null) {
+                Log.d("MAIN_ACTIVITY", "Navigating to auth activity")
             }
         })
     }
--- app/src/main/java/com/blogapp/ui/main/blogs/BlogFragment.kt
+++ app/src/main/java/com/blogapp/ui/main/blogs/BlogFragment.kt
@@ -3,7 +3,9 @@ package com.blogapp.ui.main.blogs
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
+import com.blogapp.R
 
 import com.blogapp.databinding.FragmentBlogBinding
 
@@ -15,6 +17,10 @@ class BlogFragment : BaseBlogFragment<FragmentBlogBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.goViewBlogFragment.setOnClickListener {
+            findNavController().navigate(R.id.action_blogFragment_to_viewBlogFragment)
+        }
     }
 
 }
